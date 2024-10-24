97d124a46d2da2dc4f1d5262f5adb17e7dcc6abe
randyAhmSya <randysaputra60457@gmail.com>
2024-10-24 19:22:07 +0700

    2

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -53,17 +53,14 @@ class _RegisterPageState extends State<RegisterPage> {
 
   void _submitForm() {
     if (_formKey.currentState!.validate()) {
-      // Membuat objek user baru
       User newUser = User(
         name: _nameController.text,
         email: _emailController.text,
         password: _passwordController.text,
       );
 
-      // Menambahkan user baru ke UserData
       UserData.addUser(newUser);
 
-      // Kembali ke halaman login
       Navigator.pop(context);
     }
   }
@@ -71,161 +68,162 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Daftar',
-            style: TextStyle(
-              fontSize: 24,
-              color: Colors.blue,
+      appBar: AppBar(
+        title: Text(
+          'Daftar',
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.blue,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+      ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('image.png'),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-          backgroundColor: Colors.transparent,
-        ),
-        body: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('image.png'),
-                  fit: BoxFit.cover,
+          Center(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(20.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.person,
+                      size: 100,
+                      color: Colors.blue,
+                    ),
+                    TextFormField(
+                      controller: _nameController,
+                      decoration: InputDecoration(
+                        labelText: 'Nama Lengkap',
+                        border: OutlineInputBorder(),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                            width: 2.0,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                            width: 2.0,
+                          ),
+                        ),
+                      ),
+                      validator: _validateName,
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                            width: 2.0,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                            width: 2.0,
+                          ),
+                        ),
+                      ),
+                      validator: _validateEmail,
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      controller: _passwordController,
+                      obscureText: passToggle,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                            width: 2.0,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                            width: 2.0,
+                          ),
+                        ),
+                        suffixIcon: InkWell(
+                          onTap: () {
+                            setState(() {
+                              passToggle = !passToggle;
+                            });
+                          },
+                          child: Icon(passToggle
+                              ? Icons.visibility
+                              : Icons.visibility_off),
+                        ),
+                      ),
+                      validator: _validatePassword,
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      controller: _confirmPasswordController,
+                      obscureText: confirmpassToggle,
+                      decoration: InputDecoration(
+                        labelText: 'Konfirmasi Password',
+                        border: OutlineInputBorder(),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                            width: 2.0,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                            width: 2.0,
+                          ),
+                        ),
+                        suffixIcon: InkWell(
+                          onTap: () {
+                            setState(() {
+                              confirmpassToggle = !confirmpassToggle;
+                            });
+                          },
+                          child: Icon(confirmpassToggle
+                              ? Icons.visibility
+                              : Icons.visibility_off),
+                        ),
+                      ),
+                      validator: _validateConfirmPassword,
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: _submitForm,
+                      child: Text('Daftar'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context, '/');
+                      },
+                      child: Text('Sudah punya akun? Login'),
+                    ),
+                  ],
                 ),
               ),
             ),
-            Center(
-              child: SingleChildScrollView(
-                padding: EdgeInsets.all(20.0),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.person,
-                        size: 100,
-                        color: Colors.blue,
-                      ),
-                      TextFormField(
-                        controller: _nameController,
-                        decoration: InputDecoration(
-                          labelText: 'Nama Lengkap',
-                          border: OutlineInputBorder(),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.white,
-                              width: 2.0,
-                            ),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.white,
-                              width: 2.0,
-                            ),
-                          ),
-                        ),
-                        validator: _validateName,
-                      ),
-                      SizedBox(height: 20),
-                      TextFormField(
-                        controller: _emailController,
-                        decoration: InputDecoration(
-                          labelText: 'Email',
-                          border: OutlineInputBorder(),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.white,
-                              width: 2.0,
-                            ),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.white,
-                              width: 2.0,
-                            ),
-                          ),
-                        ),
-                        validator: _validateEmail,
-                      ),
-                      SizedBox(height: 20),
-                      TextFormField(
-                        controller: _passwordController,
-                        obscureText: passToggle,
-                        decoration: InputDecoration(
-                          labelText: 'Password',
-                          border: OutlineInputBorder(),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.white,
-                              width: 2.0,
-                            ),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.white,
-                              width: 2.0,
-                            ),
-                          ),
-                          suffixIcon: InkWell(
-                            onTap: () {
-                              setState(() {
-                                passToggle = !passToggle;
-                              });
-                            },
-                            child: Icon(passToggle
-                                ? Icons.visibility
-                                : Icons.visibility_off),
-                          ),
-                        ),
-                        validator: _validatePassword,
-                      ),
-                      SizedBox(height: 20),
-                      TextFormField(
-                        controller: _confirmPasswordController,
-                        obscureText: confirmpassToggle,
-                        decoration: InputDecoration(
-                          labelText: 'Konfirmasi Password',
-                          border: OutlineInputBorder(),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.white,
-                              width: 2.0,
-                            ),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.white,
-                              width: 2.0,
-                            ),
-                          ),
-                          suffixIcon: InkWell(
-                            onTap: () {
-                              setState(() {
-                                confirmpassToggle = !confirmpassToggle;
-                              });
-                            },
-                            child: Icon(confirmpassToggle
-                                ? Icons.visibility
-                                : Icons.visibility_off),
-                          ),
-                        ),
-                        validator: _validateConfirmPassword,
-                      ),
-                      SizedBox(height: 20),
-                      ElevatedButton(
-                        onPressed: _submitForm,
-                        child: Text('Daftar'),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pop(context, '/');
-                        },
-                        child: Text('Sudah punya akun? Login'),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            )
-          ],
-        ));
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/models/data_user.dart
+++ lib/models/data_user.dart
@@ -3,18 +3,16 @@ import 'package:flutter_login/models/model_user.dart';
 class UserData {
   static List<User> registeredUsers = [];
 
-  // Method untuk menambahkan pengguna baru
   static void addUser(User user) {
     registeredUsers.add(user);
   }
 
-  // Method untuk mengecek pengguna berdasarkan email dan password
   static User? loginUser(String email, String password) {
     try {
       return registeredUsers.firstWhere(
           (user) => user.email == email && user.password == password);
     } catch (e) {
-      return null; // Kembalikan null jika pengguna tidak ditemukan
+      return null;
     }
   }
 }
